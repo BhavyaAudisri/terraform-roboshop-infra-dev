8bad6725dccf171e7df98f60d118dd153d1d7ad7
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-07-09 19:21:27 +0530

    terraform-roboshop

--- 00-vpc/variables.tf
+++ 00-vpc/variables.tf
@@ -17,12 +17,3 @@ variable "private_subnet_cidrs" {
 variable "database_subnet_cidrs" {
   default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
-
-variable "vpc_cidr" {
-
-}
-
-variable "common_tags" {
-  type = map(any)
-  #default = {}
-}
