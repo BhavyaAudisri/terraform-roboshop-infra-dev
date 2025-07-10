8e4deb8a317cdd36ebb06ea0f8b9a0b8eb965878
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-07-10 19:41:55 +0530

    terraform-roboshop

--- 80-user/provider.tf
+++ 80-user/provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "84s-remote-state-dev"
-    key    = "roboshop-dev-user"
+    bucket = "somi-tf-robo-dev"
+    key    = "tf-robo-dev-user"
     region = "us-east-1"
     encrypt        = true
     use_lockfile = true
